37f313c1e1c331a3f187bde6a657816ddea8f44c
plebhash <plebhash@proton.me>
2025-01-14 15:34:57 -0300

    [MG repo split] bring test/message-generator/{messages,mock} to repo root..
    good so we can re-use messages and mocks in the future
    
    we also delete the rest of the test directory

--- test/message-generator/test/jds-setupconnection-without-async-support/jds-setupconnection-without-async-support.sh
+++
@@ -1,9 +0,0 @@
-cd roles
-cargo llvm-cov --no-report -p pool_sv2
-
-cd ../utils/message-generator/
-cargo build
-
-RUST_LOG=debug cargo run ../../test/message-generator/test/jds-setupconnection-without-async-support/jds-setupconnection-without-async-support.json || { echo 'mg test failed' ; exit 1; }
-
-sleep 10
